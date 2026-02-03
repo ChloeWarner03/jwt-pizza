a877f2f4e36767f9e9b725d15ffb887ae003183c
“Chloe <cwarner0@byu.edu>
2026-02-03 16:28:08 -0700

    ran the tests and added the few iwth the marker

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,13 +2,23 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
+  await page.getByText('Order nowMost amazing pizza').click();
   await page.getByRole('link', { name: 'Register' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).fill('John');
+  await page.getByRole('textbox', { name: 'Full name' }).fill('Devin');
   await page.getByRole('textbox', { name: 'Email address' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('john@gmail.com');
+  await page.getByRole('textbox', { name: 'Email address' }).fill('devin@gmail.com');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('password');
-  await page.getByRole('button', { name: 'Register' }).click();  
+  await page.getByRole('textbox', { name: 'Password' }).fill('passwrod');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole('button', { name: 'Order now' }).click();
+  await page.getByRole('combobox').selectOption('20');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.getByRole('button', { name: 'Pay now' }).click();
+  await page.getByRole('button', { name: 'Verify' }).click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await page.getByRole('button', { name: 'Order more' }).click();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();;
+  await page.getByRole('link', { name: 'Logout' }).click();
 });
 
