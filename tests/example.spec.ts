f8102d605dd92dd4d495abbd400bda0798e90bf7
“Chloe <cwarner0@byu.edu>
2026-02-03 16:12:32 -0700

    made little changes ot do it to the pizza factory in the env development

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await expect(page.getByText('Welcome to Vite')).toBeVisible();
-
+  
+  
 });
