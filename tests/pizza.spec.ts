339211e4d251f8c5eb8de2c4f9128206d6862ce5
“Chloe <cwarner0@byu.edu>
2026-02-05 15:45:28 -0700

    making hcnages and adding things ot me test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -2,23 +2,14 @@ import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.getByText('Order nowMost amazing pizza').click();
-  await page.getByRole('link', { name: 'Register' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).fill('Devin');
-  await page.getByRole('textbox', { name: 'Email address' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('devin@gmail.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('passwrod');
-  await page.getByRole('button', { name: 'Register' }).click();
-  await page.getByRole('button', { name: 'Order now' }).click();
-  await page.getByRole('combobox').selectOption('20');
-  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
-  await page.getByRole('button', { name: 'Checkout' }).click();
-  await page.getByRole('button', { name: 'Pay now' }).click();
-  await page.getByRole('button', { name: 'Verify' }).click();
-  await page.getByRole('button').filter({ hasText: /^$/ }).click();
-  await page.getByRole('button', { name: 'Order more' }).click();
-  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();;
-  await page.getByRole('link', { name: 'Logout' }).click();
 });
 
+test('login', async ({ page }) => {
+  await page.route('*/**/api/auth', async (route) => {
+      const loginReq = { email: 'devin@gmail.com', password: 'password' };
+      const loginRes = {
+        user: { id: 1, name: 'Devin', email: 'devin@gmail.com', roles: [{ role: 'diner'}] },
+      }
+      token: 'fake-login',
+  },
+    
